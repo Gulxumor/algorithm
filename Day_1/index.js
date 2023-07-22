449ff3791a981f118fa81eec38df0cbddab0cd8a
Gulxumor <gulxumorerkinjonova2005@gmail.com>
2023-07-22 17:19:48 +0500

    answers to the questions were written

---
+++ Day_1/index.js
@@ -0,0 +1,67 @@
+/*//TODO
+* Difference between div and span ? 
+
+Div => block tag. Contentning width'i qancha bo'lishidan qat'iy nazar butun 1 qatorni o'ziga oladi. Keyingi kontent esa pastdan chiqadi
+Span => inline tag. Content o'ziga kerakli joyni oladi, ortib qolgan joyni esa keyingi kontentga beradi.
+
+* Location line tag link and script ?
+* Difference between null and undefined ?
+
+null => keyinroq ishlatish uchun band qilib qo'yilgan o'zgaruvchi
+undefined => o'zgaruvchi bor,lekin qiymat biriktirilmagan
+
+* What is NaN ?
+
+NaN => stands for not a number. Number type'da notogiri logika ketgan payt chiqadigan error nomi
+
+* Difference between == and === ?
+
+== => o'zgaruvchilarning tengligini tekshiradi
+=== => o'zgaruvchilarning data type'ini tengligini ham tekshiradi
+
+* Problem (0.1 + 0. 2)  === 0.3 ?
+
+Solution: false. chunki 0.1 + 0.2 qo'shilgandagi natija exactly 0.3 chiqmaydi (0.30000000000000004)
+
+*/
+
+/*
+    Problem: В функцию передается массив, функция должна вернуть сумму всех нечетных чисел больше нуля.
+*/
+
+/*
+ Crazy console:    
+*/
+
+/*
+    Problem: В функцию передается массив целых чисел и число k. Необходимо написать функцию, которая вернет булево значение,
+             true в случае если в переданном массиве есть два числа, сума которых равна k, иначе false.
+*/
+
+/*
+    Problem: В функцию передается целое положительное число. Необходимо сделать так, чтобы функция возвращала сумму всех цифр в переданном числе.
+*/
+
+/*
+    Problem: функция func принимает массив чисел, необходимо реализовать функцию так, чтобы она вернула количество элементов переданного массива,
+             которые отличны от наибольшего элемента не более чем на 10%.
+*/
+
+/*
+    Problem: Дана функция, в нее передается массив и целое положительное число. Необходимо реализовать функцию так,
+             чтобы она в массив добавляла нули до нужного размера, размер передан вторым параметром.
+*/
+
+
+/*
+    Problem: Реализовать функцию unique, в которую передается массив с числами, а функция возвращает массив, очишенный от дублей.
+*/
+
+/*
+    Problem: Реализовать функцию round5, которой передается целое число, функция должна возвращать ближайшее целое число к тому что было передано, без остатка делящееся на 5.
+*/
+
+/*
+    Problem: Необходмо реализовать функцию uniquePoints, функция принимает массив с объектами, в которых записаны точки на осях координат.
+             Необходимо сделать так, чтобы из функции возвращался массив, очищенный от дублей точек.
+*/
